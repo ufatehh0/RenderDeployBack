19fbc8742ba596bd18f976cd7ea6470cb0a294a9
ufatehh0 <ufateh2006@gmail.com>
2026-01-25 03:45:06 +0400

    admin

---
+++ DTO/AdminUpdateDto.cs
@@ -0,0 +1,15 @@
+﻿namespace CodeDungeonAPI.DTOs
+{
+    public class AdminUpdateDto
+    {
+        // User cədvəli üçün
+        public string Name { get; set; }
+        public string Email { get; set; }
+
+        // UserInfo cədvəli üçün
+        public int UserLevel { get; set; }
+        public int UserScore { get; set; }
+        public int CurrentGameLevel { get; set; }
+        public string? ProfilePictureUrl { get; set; }
+    }
+}
