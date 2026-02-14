5123f95361089bcee15054238c3f611460b3061a
ufatehh0 <ufateh2006@gmail.com>
2026-02-14 04:09:27 +0400

    edit

--- CodeDungeonEnd/Program.cs
+++ CodeDungeonEnd/Program.cs
@@ -93,7 +93,7 @@ var app = builder.Build();
 // Render'da test edebilmek için Swagger'ı Production'da da açtık (İstersen kapatabilirsin)
 app.UseSwagger();
 app.UseSwaggerUI(c => {
-    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Formix API v1");
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "CodeDungeon API v1");
     c.RoutePrefix = string.Empty; // Swagger'ı ana sayfa yapar (render-url.com/)
 });
 
